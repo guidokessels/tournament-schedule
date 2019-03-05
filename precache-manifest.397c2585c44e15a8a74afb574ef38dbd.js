102bf4a8365f1f25d2369520e7d938869463ee5c
Guido Kessels <guidokessels@gmail.com>
2019-03-05 22:12:02 +0100

    Updates

---
+++ precache-manifest.397c2585c44e15a8a74afb574ef38dbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9e2cc92a59e70cb912b",
+    "url": "/tournament-schedule/static/js/runtime~main.b9e2cc92.js"
+  },
+  {
+    "revision": "072c0cd0220d22f08750",
+    "url": "/tournament-schedule/static/js/main.072c0cd0.chunk.js"
+  },
+  {
+    "revision": "4bc44a55d45adbf93910",
+    "url": "/tournament-schedule/static/js/2.4bc44a55.chunk.js"
+  },
+  {
+    "revision": "072c0cd0220d22f08750",
+    "url": "/tournament-schedule/static/css/main.f870db5c.chunk.css"
+  },
+  {
+    "revision": "4bc44a55d45adbf93910",
+    "url": "/tournament-schedule/static/css/2.a41caf64.chunk.css"
+  },
+  {
+    "revision": "ed4a517a94d1cf9974eccaffc5010d5d",
+    "url": "/tournament-schedule/index.html"
+  }
+];
